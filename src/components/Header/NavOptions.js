dcc8561fddab532771399f6cdadf2784548e1f25
Neha yadav <yadavneha0401@gmail.com>
2024-08-09 01:52:06 +0530

    changes in the navOptions

--- src/components/Header/NavOptions.js
+++ src/components/Header/NavOptions.js
@@ -1,9 +1,20 @@
-import React, { Fragment } from 'react';
+import React, { Fragment, useState } from 'react';
 import { Link } from 'react-router-dom';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faUserCircle } from '@fortawesome/free-solid-svg-icons';
 
-const NavOptions = ({ displayMenu,toggleMenu }) => {
+const NavOptions = ({ displayMenu, toggleMenu }) => {
+  const [navDropDown, setNavDropDown] = useState({
+    readyToWear: false,
+    lookBook: false
+  });
+
+  function toggleDropDown(dropdownName) {
+    setNavDropDown(prev => ({
+      ...prev,
+      [dropdownName]: !prev[dropdownName]
+    }));
+  }
 
   return (
     <Fragment>
@@ -13,13 +24,13 @@ const NavOptions = ({ displayMenu,toggleMenu }) => {
             <FontAwesomeIcon icon={faUserCircle} className='faUserCirlce-nav' />
           </Link>
         </li>
-        <li><Link to='/newin' onClick={toggleMenu} >NEW IN</Link></li>
+        <li><Link to='/newin' onClick={toggleMenu}>NEW IN</Link></li>
         <li className="displayOnSmallerScreen"><Link to='/customer-service' onClick={toggleMenu}>Customer Service</Link></li>
         <li><Link to='/brands' onClick={toggleMenu}>BRAND</Link></li>
         <li><Link to='/aboutus' onClick={toggleMenu}>ABOUT US</Link></li>
         <li><Link to='/membership' onClick={toggleMenu}>MEMBERSHIP</Link></li>
-        <li>READY-TO-WEAR
-          {/* <ul className='dropdownOptions'>
+        <li onClick={() => toggleDropDown('readyToWear')}>READY-TO-WEAR
+          {navDropDown.readyToWear && <ul className='dropdownOptions'>
             <li>VIEW ALL</li>
             <li>DRESSES</li>
             <li>KURTIS</li>
@@ -31,19 +42,19 @@ const NavOptions = ({ displayMenu,toggleMenu }) => {
             <li>SHORTS-SKIRTS</li>
             <li>SPORTSWEAR & SWIMWEAR</li>
             <li>PANTS & JUMPSUITS</li>
-          </ul> */}
+          </ul>}
         </li>
-        <li>LOOKBOOK
-          {/* <ul className="dropdownOptions">
+        <li onClick={() => toggleDropDown('lookBook')}>LOOKBOOK
+          {navDropDown.lookBook && <ul className="dropdownOptions">
             <li>TRADITIONAL</li>
             <li>WESTERN</li>
             <li>INDO-WESTERN</li>
             <li>SPORTS</li>
-          </ul> */}
+          </ul>}
         </li>
       </ul>
     </Fragment>
-  )
+  );
 }
 
 export default NavOptions;
