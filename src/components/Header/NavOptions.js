49e1cad18b32ddde5e88cae8771cc99fff89222b
Neha yadav <yadavneha0401@gmail.com>
2024-08-25 14:17:59 +0530

    changed little bit of dropdown css and function of dropdown

--- src/components/Header/NavOptions.js
+++ src/components/Header/NavOptions.js
@@ -1,8 +1,7 @@
-import React, { Fragment, useState } from 'react';
+import React, { Fragment, useState, useEffect, useRef } from 'react';
 import { Link } from 'react-router-dom';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { faUserCircle,faCaretDown,faCaretUp } from '@fortawesome/free-solid-svg-icons';
-
+import { faUserCircle, faCaretDown, faCaretUp } from '@fortawesome/free-solid-svg-icons';
 
 const NavOptions = ({ displayMenu, toggleMenu }) => {
   const [navDropDown, setNavDropDown] = useState({
@@ -10,14 +9,39 @@ const NavOptions = ({ displayMenu, toggleMenu }) => {
     lookBook: false
   });
 
+  const dropdownRefs = {
+    readyToWear: useRef(),
+    lookBook: useRef()
+  };
+
   function toggleDropDown(dropdownName) {
-    setNavDropDown(prev => 
-    ({
+    setNavDropDown(prev => ({
       ...prev,
       [dropdownName]: !prev[dropdownName]
-    })
-  );
+    }));
   }
+
+  useEffect(() => {
+    function handleClickOutside(event) {
+      for (let dropdown in dropdownRefs) {
+        if (
+          navDropDown[dropdown] && 
+          dropdownRefs[dropdown].current && 
+          !dropdownRefs[dropdown].current.contains(event.target)
+        ) {
+          setNavDropDown(prev => ({
+            ...prev,
+            [dropdown]: false
+          }));
+        }
+      }
+    }
+
+    document.addEventListener('mousedown', handleClickOutside);
+    return () => {
+      document.removeEventListener('mousedown', handleClickOutside);
+    };
+  }, [navDropDown]);
 
   return (
     <Fragment>
@@ -32,30 +56,46 @@ const NavOptions = ({ displayMenu, toggleMenu }) => {
         <li><Link to='/brands' onClick={toggleMenu}>BRAND</Link></li>
         <li><Link to='/aboutus' onClick={toggleMenu}>ABOUT US</Link></li>
         <li><Link to='/membership' onClick={toggleMenu}>MEMBERSHIP</Link></li>
-        <li onClick={() => toggleDropDown('readyToWear')}>READY-TO-WEAR<span><FontAwesomeIcon icon={navDropDown.readyToWear?faCaretDown:faCaretUp} style={{position:'relative',bottom:'-2px',left:'3px'}}/></span>
-          {navDropDown.readyToWear && 
-          <ul className='dropdownOptions'>
-            <li>VIEW ALL</li>
-            <li>DRESSES</li>
-            <li>KURTIS</li>
-            <li>LEHENGA</li>
-            <li>SAREE</li>
-            <li>BLAZERS</li>
-            <li>TOPS-T-SHIRTS</li>
-            <li>DENIM</li>
-            <li>SHORTS-SKIRTS</li>
-            <li>SPORTSWEAR & SWIMWEAR</li>
-            <li>PANTS & JUMPSUITS</li>
-          </ul>}
+        <li ref={dropdownRefs.readyToWear} onClick={() => toggleDropDown('readyToWear')}>
+          READY-TO-WEAR
+          <span>
+            <FontAwesomeIcon
+              icon={navDropDown.readyToWear ? faCaretDown : faCaretUp}
+              style={{ position: 'relative', bottom: '-2px', left: '3px' }}
+            />
+          </span>
+          {navDropDown.readyToWear && (
+            <ul className='dropdownOptions'>
+              <li>VIEW ALL</li>
+              <li>DRESSES</li>
+              <li>KURTIS</li>
+              <li>LEHENGA</li>
+              <li>SAREE</li>
+              <li>BLAZERS</li>
+              <li>TOPS-T-SHIRTS</li>
+              <li>DENIM</li>
+              <li>SHORTS-SKIRTS</li>
+              <li>SPORTSWEAR & SWIMWEAR</li>
+              <li>PANTS & JUMPSUITS</li>
+            </ul>
+          )}
         </li>
-        <li onClick={() => toggleDropDown('lookBook')}>LOOKBOOK<span><FontAwesomeIcon icon={navDropDown.lookBook?faCaretDown:faCaretUp} style={{position:'relative',bottom:'-2px',left:'3px'}}/></span>
-          {navDropDown.lookBook && 
-          <ul className="dropdownOptions">
-            <li>TRADITIONAL</li>
-            <li>WESTERN</li>
-            <li>INDO-WESTERN</li>
-            <li>SPORTS</li>
-          </ul>}
+        <li ref={dropdownRefs.lookBook} onClick={() => toggleDropDown('lookBook')}>
+          LOOKBOOK
+          <span>
+            <FontAwesomeIcon
+              icon={navDropDown.lookBook ? faCaretDown : faCaretUp}
+              style={{ position: 'relative', bottom: '-2px', left: '3px' }}
+            />
+          </span>
+          {navDropDown.lookBook && (
+            <ul className="dropdownOptions">
+              <li>TRADITIONAL</li>
+              <li>WESTERN</li>
+              <li>INDO-WESTERN</li>
+              <li>SPORTS</li>
+            </ul>
+          )}
         </li>
       </ul>
     </Fragment>
